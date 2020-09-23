80b89c63a29a06edf234c2b74e33765ab6bc92c4
Roy Luo <r.luo2418@gmail.com>
2020-09-23 15:59:59 +1200

    facelift on UI

--- public/lecturerView.js
+++ public/lecturerView.js
@@ -1,16 +1,24 @@
+// emojis 
+var emoji = {
+    '🧐': "focused (neutral)",
+    '😁': "happy (content is easy)",
+    '😴': "sleepy (lecture is boring)", 
+    '🙁': "struggling (slow down please)", 
+    '🤯': "completely lost (please explain again)"
+}
 // make connection 
 var socket = io.connect("http://localhost:4000"); 
 
 // Query DOM 
-var output = document.getElementById('studentFeelings'); 
+var output = document.getElementById('tableBody'); 
 
 // Listen to events 
 socket.on('studentName', function(data) {
-    output.innerHTML += "<div id=" + data.id + ">" + data.name + "🤩" + "</div>"; 
+    output.innerHTML += "<tr id=" + data.id + ">" + "<td>" + data.name + "</td><td>" + "🧐" + "</td></tr>";
 });
 
 socket.on('studentFeeling', function(data) {
     var student = document.getElementById(data.id); 
     student.innerHTML = ""; 
-    student.innerHTML = data.name + data.feeling;
+    student.innerHTML = "<td>" + data.name + "</td><td>" + data.feeling + "</td>";
 });
--- public/studentView.js
+++ public/studentView.js
@@ -1,26 +1,40 @@
+// emojis 
+var emoji = {
+    '🧐': "focused (neutral)",
+    '😁': "happy (content is easy)",
+    '😴': "sleepy (lecture is boring)", 
+    '🙁': "struggling (slow down please)", 
+    '🤯': "completely lost (please explain again)"
+}
+// user information 
+var name;
+var feeling = "🧐";
 // make connection 
 var socket = io.connect("http://localhost:4000"); 
-
 // Query DOM 
 var submit = document.getElementById('submit'); 
-var happy = document.getElementById('happy'); 
-var sad = document.getElementById('sad'); 
-var crying = document.getElementById('crying'); 
-
+var expressFeeling = document.getElementById('expressFeeling');
 // Handle button events 
 submit.addEventListener('click', function() {
-    var name = document.getElementById('studentName'); 
+    name = document.getElementById('studentName').value; 
     socket.emit('studentName', {
-        name: name.value,
+        name: name,
         id: socket.id
     })
+    document.getElementById('join').remove();
+    document.getElementById('nameTitle').innerHTML = "You are: " + name;
 });
 
-happy.addEventListener('click', function() {
-    var name = document.getElementById('studentName'); 
+expressFeeling.addEventListener('click', function() {
+    var radios = document.getElementsByName('radio'); 
+    for (var i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            feeling = radios[i].value; 
+        }
+    }
     socket.emit('studentFeeling', {
-        feeling: happy.value,
+        feeling: feeling,
         id: socket.id,
-        name: name.value
+        name: name
     })
 });
